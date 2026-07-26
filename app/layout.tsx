f16c9b57c0d249079ad75bf4da7bb4cef5c04f8e
Chinmay <chinuchoudhary2006@gmail.com>
2026-07-26 17:55:12 +0530

    feat: add new UI components including Skeleton, Slider, Spinner, Switch, Table, Tabs, Textarea, Toast, Toggle Group, and Toggle; implement mobile detection hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Oxanium } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const oxanium = Oxanium({subsets:['latin'],variable:'--font-sans'});
 
@@ -29,8 +30,16 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", oxanium.variable)}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >{children}
+        </ThemeProvider></body>
     </html>
   );
 }
