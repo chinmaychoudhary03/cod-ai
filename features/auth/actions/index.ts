31250e7d7f0e7e1d62a6b306e18b3d3d99726515
Chinmay <chinuchoudhary2006@gmail.com>
2026-07-27 20:24:19 +0530

    feat: implement authentication flow; add user session management, protected routes, and user menu component

--- features/auth/actions/index.ts
+++ features/auth/actions/index.ts
@@ -2,15 +2,18 @@
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
 import {redirect} from "next/navigation";
+import { DEFAULT_AUTH_CALLBACK,getSafeCallbackPath,SIGN_IN_PATH } from "../utils";
 
 export async function signInWithGithub(formData:FormData){
     const callback=formData.get("callbackUrl");
 
-    //todo :fix callback later
+    const redirectTo=getSafeCallbackPath(
+        typeof callback==="string"? callback:null
+    );
     const result=await auth.api.signInSocial({
         body:{
             provider:"github",
-            callbackURL:"/dashboard"
+            callbackURL: redirectTo
         },
         headers:await headers()
 
@@ -18,4 +21,30 @@ export async function signInWithGithub(formData:FormData){
     if(result.url){
         redirect(result.url)
     }
+}
+
+export async function getServerSession(){
+    return auth.api.getSession({
+        headers:await headers()
+
+    })
+
+    
+}
+export async function requireAuth(redirectTo=SIGN_IN_PATH){
+    const session=await getServerSession();
+    if(!session){
+        redirect(redirectTo)
+    }
+    return session;
+
+}
+
+export async function requireUnAuth(redirectTo = DEFAULT_AUTH_CALLBACK) {
+    const session = await getServerSession();
+    if (session) {
+        redirect(redirectTo)
+    }
+    
+
 }
